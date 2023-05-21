242fcf00fb459da140e31a2b58923675c8f0c6c9
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-05-21 12:51:34 +0100

    feat : configure commerce platform config server

--- src/main/java/com/commerceplatform/commerceplatformconfigserver/CommercePlatformConfigServerApplication.java
+++ src/main/java/com/commerceplatform/commerceplatformconfigserver/CommercePlatformConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.commerceplatform.commerceplatformconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CommercePlatformConfigServerApplication {
 
 	public static void main(String[] args) {
