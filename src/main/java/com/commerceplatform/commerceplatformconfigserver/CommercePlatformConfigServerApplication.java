870d940f5465cd4003b6027e444f3c0fd27637a8
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-05-21 12:37:44 +0100

    feat : init project

---
+++ src/main/java/com/commerceplatform/commerceplatformconfigserver/CommercePlatformConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.commerceplatform.commerceplatformconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePlatformConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercePlatformConfigServerApplication.class, args);
+	}
+
+}
